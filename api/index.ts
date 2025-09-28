b6421ee50f65a9fd04f5609cbbc348c8095d9715
Pietro Porsch Wilhelms <97356900+Pietro-Porsch-W@users.noreply.github.com>
2025-09-28 20:43:45 -0300

    Correção de deploy para o vercel

---
+++ api/index.ts
@@ -0,0 +1,3 @@
+// api/index.ts
+import app from '../app';
+export default app;
--- app.ts
+++ app.ts
@@ -1,19 +1,23 @@
+// app.ts
 import express from 'express';
 import cors from 'cors';
 import routes from './src/routes/index';
 
 const app = express();
 
-app.use(cors());
+app.use(cors()); // depois podemos restringir para o domínio do front
 app.use(express.json());
 
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
-	console.log('Environment:');
-	console.log(process.env.DATABASE);
-});
+// só liga servidor quando NÃO estiver na Vercel
+if (process.env.VERCEL !== '1') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+    console.log('Environment:');
+    console.log(process.env.DATABASE);
+  });
+}
 
 export default app;
